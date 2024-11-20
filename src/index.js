3cdf890866ab604f3dac7b82a2113bd318f351f4
rjsaimon-gif <sreginajilta@gmail.com>
2024-11-19 21:48:19 -0600

    added functionality for the select

--- src/index.js
+++ src/index.js
@@ -1,33 +1,60 @@
-function updateTime (){
+function updateTime() {
+  let losAngelesElement = document.querySelector("#los-angeles");
+  if (losAngelesElement) {
+    let losAngelesDateElement = losAngelesElement.querySelector(".date");
+    let losAngelesTimeElement = losAngelesElement.querySelector(".time");
+    let losAngelesTime = moment().tz("America/Los_Angeles");
 
-let milwaukeeElement = document.querySelector("#milwaukee");
-let milwaukeeDateElement = milwaukeeElement.querySelector(".date");
-let milwaukeeTimeElement = milwaukeeElement.querySelector(".time");
-let milwaukeeTime = moment().tz("America/Milwaukee");
+    losAngelesDateElement.innerHTML = losAngelesTime.format("MMMM Do YYYY");
+    losAngelesTimeElement.innerHTML = losAngelesTime.format(
+      "h:mm:ss [<small>]A[</small>]"
+    );
+  }
 
-milwaukeeDateElement.innerHTML = milwaukeeTime.format("MMMM Do YYYY");
-milwaukeeTimeElement.innerHTML = milwaukeeTime.format("h:mm:ss [<small>]A[</small>]"); 
+  let pohnpeiElement = document.querySelector("#pohnpei");
+  if (pohnpeiElement) {
+    let pohnpeiDateElement = pohnpeiElement.querySelector(".date");
+    let pohnpeiTimeElement = pohnpeiElement.querySelector(".time");
+    let pohnpeiTime = moment().tz("Pacific/Pohnpei");
 
-
-
-let pohnpeiElement = document.querySelector("#pohnpei");
-let pohnpeiDateElement = pohnpeiElement.querySelector(".date");
-let pohnpeiTimeElement = pohnpeiElement.querySelector(".time");
-let pohnpeiTime = moment().tz("Pacific/Pohnpei");
-
-pohnpeiDateElement.innerHTML = pohnpeiTime.format("MMMM Do YYYY");
-pohnpeiTimeElement.innerHTML = pohnpeiTime.format("h:mm:ss [<small>]A[</small>]"); 
-
+    pohnpeiDateElement.innerHTML = pohnpeiTime.format("MMMM Do YYYY");
+    pohnpeiTimeElement.innerHTML = pohnpeiTime.format(
+      "h:mm:ss [<small>]A[</small>]"
+    );
+  }
 
   let seoulElement = document.querySelector("#seoul");
-  let seoulDateElement = seoulElement.querySelector(".date");
-  let seoulTimeElement = seoulElement.querySelector(".time");
-  let seoulTime = moment().tz("Asiz/Seoul");
+  if (seoulElement) {
+    let seoulDateElement = seoulElement.querySelector(".date");
+    let seoulTimeElement = seoulElement.querySelector(".time");
+    let seoulTime = moment().tz("Asia/Seoul");
 
-  seoulDateElement.innerHTML = seoulTime.format("MMMM Do YYYY");
-  seoulTimeElement.innerHTML = seoulTime.format(
-    "h:mm:ss [<small>]A[</small>]"
-  );}
+    seoulDateElement.innerHTML = seoulTime.format("MMMM Do YYYY");
+    seoulTimeElement.innerHTML = seoulTime.format(
+      "h:mm:ss [<small>]A[</small>]"
+    );
+  }
+}
+
+function updateCity(event) {
+  let cityTimeZone = event.target.value;
+  let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+  let cityTime = moment().tz(cityTimeZone);
+  let citiesElement = document.querySelector("#cities");
+  citiesElement.innerHTML = `
+      <div class="city">
+        <div>
+          <h2>${cityName}</h2>
+          <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
+        </div>
+        <div class="time">${cityTime.format(
+          "h:mm:ss"
+        )} <small> ${cityTime.format("A")}</small></div>
+      </div>`;
+}
+
 updateTime();
-
 setInterval(updateTime, 1000);
+
+let citiesSelectElement = document.querySelector("#city");
+citiesSelectElement.addEventListener("change", updateCity);
